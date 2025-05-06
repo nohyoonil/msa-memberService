47fafcefff6e2cd0f60736204d18f8030c81b719
yoon <yunil9631@naver.com>
2025-05-06 12:55:09 +0900

    init

---
+++ src/main/java/org/yoon/msamemberservice/MsaMemberServiceApplication.java
@@ -0,0 +1,13 @@
+package org.yoon.msamemberservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaMemberServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaMemberServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/yoon/msamemberservice/MsaMemberServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yoon.msamemberservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaMemberServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
