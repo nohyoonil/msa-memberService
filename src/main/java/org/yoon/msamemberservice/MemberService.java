2b98f02ecc184d8dcdec500d989a921dcf2c6809
yoon <yunil9631@naver.com>
2025-05-06 14:51:50 +0900

    signIn jwt 토큰 생성

--- src/main/java/org/yoon/msamemberservice/MemberService.java
+++ src/main/java/org/yoon/msamemberservice/MemberService.java
@@ -3,9 +3,12 @@ package org.yoon.msamemberservice;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.yoon.msamemberservice.jwt.JwtUtil;
 import org.yoon.msamemberservice.model.request.SignInForm;
 import org.yoon.msamemberservice.model.request.SignUpForm;
 import org.yoon.msamemberservice.model.response.MemberDetailRes;
+
+import java.time.Duration;
 
 @Service
 @RequiredArgsConstructor
@@ -13,6 +16,7 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
     private final BCryptPasswordEncoder encoder;
+    private final JwtUtil jwtUtil;
 
     public MemberDetailRes signUp(SignUpForm form) {
         Member member = memberRepository.save(Member.builder()
@@ -31,6 +35,6 @@ public class MemberService {
         if (!encoder.matches(form.getPassword(), member.getPassword()))
             throw new RuntimeException("not match");
 
-        return "todo jwt";
+        return jwtUtil.createToken(member.getId(), member.getEmail(), Duration.ofDays(7));
     }
 }
---
+++ src/main/java/org/yoon/msamemberservice/jwt/JwtUtil.java
@@ -0,0 +1,42 @@
+package org.yoon.msamemberservice.jwt;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+
+    @Value("${jwt.issuer}")
+    private String issuer;
+    @Value("${jwt.secret}")
+    private String secret;
+    private SecretKey key;
+    private final static String TOKEN_PREFIX = "Bearer ";
+
+    @PostConstruct
+    public void init() {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String createToken(Long id, String email, Duration expiration) {
+        final Date now = new Date();
+
+        return Jwts.builder()
+                .setIssuer(issuer)
+                .claim("email", email)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + expiration.toMillis())) // 1시간
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+}
