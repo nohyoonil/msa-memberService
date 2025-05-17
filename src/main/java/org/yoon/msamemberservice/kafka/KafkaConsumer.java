f08c77fa006f867d3f752368524182e5daa59db9
yoon <yunil9631@naver.com>
2025-05-17 14:45:00 +0900

    kafka + redis polling으로 변경

--- src/main/java/org/yoon/msamemberservice/kafka/KafkaConsumer.java
+++ src/main/java/org/yoon/msamemberservice/kafka/KafkaConsumer.java
@@ -1,9 +1,9 @@
 package org.yoon.msamemberservice.kafka;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,38 +11,28 @@ import org.yoon.msamemberservice.Member;
 import org.yoon.msamemberservice.MemberRepository;
 import org.yoon.msamemberservice.model.dto.VoteDetailDto;
 
-import java.util.HashMap;
+import java.time.Duration;
+
 
 @Service
 @RequiredArgsConstructor
 public class KafkaConsumer {
 
     private final ObjectMapper objectMapper;
-    private final KafkaProducer kafkaProducer;
+    private final RedisTemplate<String, String> redisTemplate;
     private final MemberRepository memberRepository;
 
     @Transactional
-    @KafkaListener(topics = "validate.member", groupId = "member-service")
+    @KafkaListener(topics = "validate.memberId", groupId = "member-service")
     public void listen(String message) {
         try {
-            VoteDetailDto voteDetailDto = objectMapper.readValue(message, VoteDetailDto.class);
-            Long voterId = voteDetailDto.getVoterId();
-            Long targetId = voteDetailDto.getTargetId();
-            int reward = voteDetailDto.getRewards();
+            Long memberId = objectMapper.readValue(message, Long.class);
+            memberRepository.findById(memberId)
+                    .orElseThrow(() -> new RuntimeException("target not found: " + memberId));
 
-            Member voter = memberRepository.findById(voterId)
-                    .orElseThrow(() -> new RuntimeException("voter not found: " + voterId));
-            Member target = memberRepository.findById(targetId)
-                    .orElseThrow(() -> new RuntimeException("target not found: " + targetId));
-
-            voter.plusPoints(reward);
-            voter.plusVoteSum();
-            target.plusVotedSum();
-
-            kafkaProducer.send("create.notification", objectMapper.writeValueAsString(voteDetailDto));
-
+            redisTemplate.opsForValue().set("memberId:" + memberId, "exist", Duration.ofSeconds(3));
         } catch (JsonProcessingException | RuntimeException e) {
-            e.printStackTrace(); // logger 사용 추천
+            e.printStackTrace();
         }
     }
 }
